0d3dbfda16bc81637693f3efab9065414bcb70b8
Eric <eric248550@gmail.com>
2023-01-20 15:35:46 -0600

    design mock minting, embed

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ export default function App() {
       {/* Routes nest inside one another. Nested route paths build upon
             parent route paths, and nested route elements render inside
             parent route elements. See the note about <Outlet> below. */}
+            
       <Routes>
             <Route path="/" element={<HomeLayout />}>
                 <Route index element={<Home />} />
